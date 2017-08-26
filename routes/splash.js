0e5c28298fc01c8e968204b5c512349772195e48
Daniel Sze <danssze@gmail.com>
2017-08-26 15:15:00 -0400

    ay

--- routes/splash.js
+++ routes/splash.js
@@ -3,9 +3,15 @@
  */
 
 exports.index = function(req, res) {
+    var issues;
+    $.ajax({
+        'url': '/issues',
+        'type': 'GET',
+        'success': function(data) {
+            issues = data;
+        }
+    });
     var template_engine = req.app.settings.template_engine;
     res.locals.session = req.session;
-    res.render('splash', {
-        title: 'SigmaHouse'
-    });
+    res.render('splash', issues);
 };
