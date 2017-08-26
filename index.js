f017fb91f6bf59d294b920bb93ce8b6c964ea555
Timothyw0 <30346660+Timothyw0@users.noreply.github.com>
2017-08-26 13:06:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Home page.
+ */
+
+exports.index = function(req, res) {
+    var template_engine = req.app.settings.template_engine;
+    res.locals.session = req.session;
+    res.render('index', {
+        title: 'SigmaHouse'
+    });
+};
