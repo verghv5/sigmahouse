4d189682eed42b4d0ad62276ba3db370527cfe07
Tim Wang <Tim@Tims-MacBook-Pro.local>
2017-08-27 10:46:56 -0400

    Added shit

--- js/login.js
+++ js/login.js
@@ -1,8 +1,25 @@
 //Validation function
 function validate(){
+    var request = require(['request']);
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username=="a" && password=="a") {
-        
-    }
+
+    var encrypted = CryptoJS.AES.encrypt(password, "BOB JONES HEH");
+    var decrypted = CryptoJS.AES.decrypt(encrypted, "BOB JONES HEH");
+    console.log(decrypted, )
+
+    var credentials = [{ "username": username }, { "password": encrypted }];
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8000/login",
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify({ Credentials: credentials }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){alert(data);},
+        failure: function(errMsg) {
+            alert(errMsg);
+        }
+    });
 }
--- routes/splash.js
+++ routes/splash.js
@@ -2,18 +2,16 @@
  * Main Display Page
  */
 
-<<<<<<< HEAD
 module.exports = function(req, res) {
-=======
-exports.index = function(req, res) {
->>>>>>> 3160f4f8acd5dbfc1e8920d548044357b2fc70b0
+    var issues;
+    $.ajax({
+        'url': '/issues',
+        'type': 'GET',
+        'success': function(data) {
+            issues = data;
+        }
+    });
     var template_engine = req.app.settings.template_engine;
     res.locals.session = req.session;
-    res.render('splash', {
-        title: 'SigmaHouse'
-    });
-<<<<<<< HEAD
+    res.render('splash', issues);
 };
-=======
-};
->>>>>>> 3160f4f8acd5dbfc1e8920d548044357b2fc70b0
