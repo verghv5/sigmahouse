2390c123f15a1a3546d3e407ac1b127365c9c29f
Tim Wang <Tim@Tims-MacBook-Pro.local>
2017-08-27 10:51:59 -0400

    Send login credentials

--- js/login.js
+++ js/login.js
@@ -5,8 +5,6 @@ function validate(){
     var password = document.getElementById("password").value;
 
     var encrypted = CryptoJS.AES.encrypt(password, "BOB JONES HEH");
-    var decrypted = CryptoJS.AES.decrypt(encrypted, "BOB JONES HEH");
-    console.log(decrypted, )
 
     var credentials = [{ "username": username }, { "password": encrypted }];
 
@@ -14,7 +12,7 @@ function validate(){
         type: "POST",
         url: "http://localhost:8000/login",
         // The key needs to match your method's input parameter (case-sensitive).
-        data: JSON.stringify({ Credentials: credentials }),
+        data: JSON.stringify({ "Username": username }, { "Password": password }),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data){alert(data);},
